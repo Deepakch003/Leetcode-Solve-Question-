52bbac5f9022800a291909e23c8f5df9fba5de2e
Deepakch003 <157952603+Deepakch003@users.noreply.github.com>
2026-07-20 23:40:41 +0530

    Time: 6 ms (55.4%), Space: 47.1 MB (64.37%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -0,0 +1,74 @@
+// class Solution {
+//     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+
+//         int m = grid.length;
+//         int n = grid[0].length;
+
+//         int total = m*n;
+
+//         k=k%total;
+
+//         List<List<Integer>> ans = new ArrayList<>();
+
+//         for(int i=0;i<m;i++){
+//           List<Integer> row = new ArrayList<>();
+//           for(int j=0;i<n;j++){
+//             row.add(0);
+//           }
+//           ans.add(row);
+//         }
+
+
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//               int   index = i*n+j;
+//             int newIndex = (index+k)%total;
+
+//             int newrow = newIndex/n;
+//             int newcol = newIndex%n;
+//             ans.get(newrow).set(newcol,grid[i][j]);
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int total = m * n;
+
+        k %= total;
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(0);
+            }
+            ans.add(row);
+        }
+
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int index = i * n + j;
+                int newIndex = (index + k) % total;
+
+                int newRow = newIndex / n;
+                int newCol = newIndex % n;
+
+                ans.get(newRow).set(newCol, grid[i][j]);
+            }
+        }
+
+        return ans;
+    }
+}
