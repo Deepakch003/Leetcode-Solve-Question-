1c4300c713057383f777fd021b41d7b09f6d0930
Deepakch003 <157952603+Deepakch003@users.noreply.github.com>
2026-07-22 14:59:23 +0530

    Time: 3 ms (57.71%), Space: 43.9 MB (31.16%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+
+        HashSet<Character> broken = new HashSet<>();
+
+        for (char ch : brokenLetters.toCharArray()) {
+            broken.add(ch);
+        }
+
+        int count = 0;
+
+        String[] words = text.split(" ");
+
+        for (String word : words) {
+            boolean ok = true;
+
+            for (char ch : word.toCharArray()) {
+                if (broken.contains(ch)) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
