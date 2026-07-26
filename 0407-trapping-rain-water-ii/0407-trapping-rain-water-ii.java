b42cb1b8bff4a4e0bc679e1abfbb42b952946d4b
Deepakch003 <157952603+Deepakch003@users.noreply.github.com>
2026-07-26 10:23:19 +0530

    Time: 18 ms (96.79%), Space: 48.2 MB (23.58%) - LeetHub

---
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -0,0 +1,63 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    class Cell {
+        int x, y, h;
+
+        Cell(int x, int y, int h) {
+            this.x = x;
+            this.y = y;
+            this.h = h;
+        }
+    }
+
+    public int trapRainWater(int[][] heightMap) {
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+
+        if (m < 3 || n < 3) return 0;
+
+        boolean[][] visited = new boolean[m][n];
+        PriorityQueue<Cell> pq = new PriorityQueue<>((a, b) -> a.h - b.h);
+
+    
+        for (int i = 0; i < m; i++) {
+            pq.offer(new Cell(i, 0, heightMap[i][0]));
+            pq.offer(new Cell(i, n - 1, heightMap[i][n - 1]));
+            visited[i][0] = true;
+            visited[i][n - 1] = true;
+        }
+
+        for (int j = 1; j < n - 1; j++) {
+            pq.offer(new Cell(0, j, heightMap[0][j]));
+            pq.offer(new Cell(m - 1, j, heightMap[m - 1][j]));
+            visited[0][j] = true;
+            visited[m - 1][j] = true;
+        }
+
+        int water = 0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while (!pq.isEmpty()) {
+            Cell cur = pq.poll();
+
+            for (int[] d : dirs) {
+                int nx = cur.x + d[0];
+                int ny = cur.y + d[1];
+
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n || visited[nx][ny])
+                    continue;
+
+                visited[nx][ny] = true;
+
+                if (heightMap[nx][ny] < cur.h) {
+                    water += cur.h - heightMap[nx][ny];
+                }
+
+                pq.offer(new Cell(nx, ny, Math.max(cur.h, heightMap[nx][ny])));
+            }
+        }
+
+        return water;
+    }
+}
