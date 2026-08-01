2c07fc9ec677e9f7cb2cd7a90f1e6ebeb27a891b
Deepakch003 <157952603+Deepakch003@users.noreply.github.com>
2026-08-01 17:50:05 +0530

    Time: 1 ms (99.61%), Space: 45.7 MB (68.51%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int[] freq = new int[100];
+        int[] ans = new int[2];
+        int idx = 0;
+
+        for (int num : nums) {
+            freq[num]++;
+            if (freq[num] == 2) {
+                ans[idx++] = num;
+            }
+        }
+
+        return ans;
+    }
+}
